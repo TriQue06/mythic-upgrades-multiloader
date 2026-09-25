6e71c8fe413da327713415c5e74c4321b74e36da
trique <baristanlik@gmail.com>
2026-09-25 12:34:09 +0300

    .

--- common/src/main/java/net/trique/mythicupgrades/mixin/MixinOverworldBiomeBuilder.java
+++
@@ -1,8 +0,0 @@
-package net.trique.mythicupgrades.mixin;
-
-import net.minecraft.world.level.biome.OverworldBiomeBuilder;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(OverworldBiomeBuilder.class)
-public class MixinOverworldBiomeBuilder {
-}
